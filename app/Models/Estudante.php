19be32dbdd4279403f6b18a1337f82fbe3267ac1
nicolau-np <69913473+nicolau-np@users.noreply.github.com>
2025-02-07 15:06:49 +0100

    feat: trabalhar nos modelos

--- app/Models/Estudante.php
+++ app/Models/Estudante.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Estudante extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'pessoa_id',
+        'turma',
+    ];
+
+    public function pessoa(): BelongsTo
+    {
+        return $this->belongsTo(Pessoa::class, 'pessoa_id');
+    }
 }
--- app/Models/Pessoa.php
+++ app/Models/Pessoa.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Pessoa extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'data_de_nascimento',
+    ];
+
+    public function estudante(): HasMany
+    {
+        return $this->hasMany(Estudante::class, 'pessoa_id');
+    }
 }
