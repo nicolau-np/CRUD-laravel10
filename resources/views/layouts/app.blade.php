19b69174bb5eaca73cadc3e339e1df13affc4c98
nicolau-np <69913473+nicolau-np@users.noreply.github.com>
2026-07-23 08:50:28 +0100

    feat: update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CRUD-laravel10</title>
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 </head>
 
 <body>
     @yield('content')
+
+    <script src="{{asset('assets/js/script.js')}}"></script>
 </body>
 
 </html>
